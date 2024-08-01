974a9556f336a37427feec5a50e2cd51f91d0655
Георгий Панасенко <georgy.panasenko@gmail.com>
2024-08-01 21:49:05 +0700

    Fix flake8 errors

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,14 @@
+"""Docstring."""
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """Docstring."""
+
     class Meta:
+        """Docstring."""
+
         model = Task
         fields = ('id', 'title', 'description', 'completed')
